d9675f8b2c39cba58bd033e4ca1bd2da0e301eda
Emmanuel PETIBON <emmanuel.petibon@epita.fr>
2021-09-29 20:09:34 +0200

    Add Dotenv

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import os
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN') 
 
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
@@ -17,5 +21,8 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "56a42d2b2f6b91e2c11d71298727ce13f8d9a9136a79349d5c4100fff759d657"
-bot.run(token)  # Starts the bot
+@bot.command()
+async def name(ctx):
+    await ctx.send('pong')
+
+bot.run(TOKEN)  # Starts the bot
