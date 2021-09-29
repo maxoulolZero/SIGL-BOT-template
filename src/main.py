531cec6d13ac886b0c57b673dfe855f66af26680
Emmanuel PETIBON <emmanuel.petibon@epita.fr>
2021-09-29 21:50:06 +0200

    Add the implementations and scope of each functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import os
+
+# --- Discord Imports --- #
 from discord.ext import commands
+from discord import Member
+
+# --- Python Imports --- #
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') 
@@ -21,8 +27,46 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
+# --------------- #
+# --- Warm Up --- #
+# --------------- #
 @bot.command()
 async def name(ctx):
     await ctx.send(ctx.message.author)
+
+# --------------------- #
+# --- Adminisration --- #
+# --------------------- #
+@bot.command()
+async def admin(ctx):
+    await ctx.send("TODO Implement admin")
+
+@bot.command()
+async def mute(ctx):
+    await ctx.send("TODO Implement mute")
+    
+@bot.command()
+async def ban(ctx, member: Member):
+    # The guild corresponds to server, the function below means we'll ban the member <member>
+    await ctx.guild.ban(member, reason="Because.")
+
+    # Log to the server that the member <member> has been banned
+    await ctx.send(f"You've banned {member} !")
+
+# ------------------------------ #
+# --- It's All Fun And Games --- #
+# ------------------------------ #
+@bot.command()
+async def xkcd(ctx):
+    number = random.randint()
+    await ctx.send("TODO Implement xkcd")
+
+@bot.command()
+async def poll(ctx):
+    await ctx.send("TODO Implement poll")
+
+@bot.command()
+async def tictactoe(ctx):
+    await ctx.send("TODO Implement tictactoe")
 
 bot.run(TOKEN)  # Starts the bot
